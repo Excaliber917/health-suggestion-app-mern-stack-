f5c0f70492bcf496bf1669ff9381e44116c5e516
Dhruba <93818182+Excaliber917@users.noreply.github.com>
2024-08-29 21:24:28 +0530

    contact us textarea

--- client/src/pages/ContactUs.jsx
+++ client/src/pages/ContactUs.jsx
@@ -72,7 +72,7 @@ function ContactUs() {
                 name="from_name" // Ensure it matches the EmailJS template variable
                 value={formData.from_name}
                 onChange={handleChange}
-                className="w-full px-3 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-pink-400 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-pink-500 transition duration-300"
+                className="w-full px-3 py-2  dark:text-white border rounded focus:outline-none focus:ring-2 focus:ring-pink-400 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-pink-500 transition duration-300"
                 required
               />
             </div>
@@ -85,7 +85,7 @@ function ContactUs() {
                 name="from_email" // Ensure it matches the EmailJS template variable
                 value={formData.from_email}
                 onChange={handleChange}
-                className="w-full px-3 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-pink-400 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-pink-500 transition duration-300"
+                className="w-full  dark:text-white px-3 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-pink-400 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-pink-500 transition duration-300"
                 required
               />
             </div>
@@ -98,7 +98,7 @@ function ContactUs() {
                 value={formData.message}
                 onChange={handleChange}
                 rows="5"
-                className="w-full px-3 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-pink-400 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-pink-500 transition duration-300"
+                className="w-full dark:text-white px-3 py-2 border rounded focus:outline-none focus:ring-2 focus:ring-pink-400 dark:bg-gray-700 dark:border-gray-600 dark:focus:ring-pink-500 transition duration-300"
                 required
               />
             </div>
