2955b42e83fa42c624bf6af3d2528b9971179e44
Dhruba <93818182+Excaliber917@users.noreply.github.com>
2024-08-30 20:34:03 +0530

    backend link added

--- client/src/hooks/useLogin.js
+++ client/src/hooks/useLogin.js
@@ -16,7 +16,7 @@ export const useLogin = ()=>{
         try{
             setLoading(true)
             // console.log(loading)
-            const res = await axios.post('/api/auth/login',{
+            const res = await axios.post('https://fithealth-aea5.onrender.com/api/auth/login',{
                 username,
                 password
             })
