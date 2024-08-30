7261334e4c97b466b63cabd4f32cda1c129e7dd7
Dhruba <93818182+Excaliber917@users.noreply.github.com>
2024-08-30 20:23:24 +0530

    bug fixed

--- server/index.js
+++ server/index.js
@@ -6,25 +6,25 @@ import cookieParser from 'cookie-parser';
 import userRoute from './routes/user.route.js';
 import messageRouter from './routes/message.route.js';
 import cors from 'cors'
+const app = express()
+dotenv.config()
 const corsOptions = {
     origin: 'https://fithealth-frontend.onrender.com', //  frontend URL
     optionsSuccessStatus: 200,
-  };
-  
-  app.use(cors(corsOptions));
-dotenv.config()
-const app = express()
+};
+
+app.use(cors(corsOptions));
 app.use(express.json());
 app.use(cookieParser())
 const port = process.env.PORT || 5001
 
-app.use("/api/auth",authRoute)
-app.use("/api/user",userRoute)
-app.use("/api/mentalhealth",messageRouter)
+app.use("/api/auth", authRoute)
+app.use("/api/user", userRoute)
+app.use("/api/mentalhealth", messageRouter)
 
 
 
-app.listen(port,()=>{
+app.listen(port, () => {
     connectDb()
     console.log(`app is running on ${port} `)
 })
